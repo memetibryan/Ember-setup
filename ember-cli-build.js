f27c2dd8e4ece07909bce403f654c02191f8c9d5
memetibryan <memetibryan@gmail.com>
2017-06-13 14:49:25 +0300

    Added google-map service to use the service in our component to return the map to the display by adding the code to google-map.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    'ember-bootstrap': {
+      'bootstrapVersion': 3,
+      'importBootstrapFont': true,
+      'importBootstrapCSS': true
+    }
   });
 
   app.import('bower_components/moment/moment.js')
