228bc082c1d181b776c296ec0c2725de9f52096d
memetibryan <memetibryan@gmail.com>
2017-06-13 14:55:06 +0300

    fixed 'mixed tabs and space' error

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
       }
     },
     destroyReview(review) {
-    	this.sendAction('destroyReview',review);
+    	this.sendAction('destroyReview', review);
     }
   }
 });
